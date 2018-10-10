cae3b516cf8df1fd1e9fff89a85dd70861308517
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-10 16:59:01 +0300

    ft(user should be able to login into the app) This commit delivers the login page that enables user to gain access to the app [Delivers #161090124]

---
+++ ui/static/js/login.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+const username = document.querySelector("#username");
+const password = document.querySelector("password")
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    if(username.value !== "admin" && password!=="password"){
+        window.location = "./user_dash.html";
+    }
+    else{
+        window.location = "./admin_dash.html"
+    }
+})
