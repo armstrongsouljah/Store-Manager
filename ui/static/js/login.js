436582039d6983859a44268a6a55b4beb7f2af74
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-10 17:32:59 +0300

    User login page
    [Delivers #161090124]

--- ui/static/js/login.js
+++ ui/static/js/login.js
@@ -4,11 +4,12 @@ const password = document.querySelector("password")
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
+    // check entered user and send them to respective dashboard
     if(username.value !== "admin" && password!=="password"){
         window.location = "./user_dash.html";
     }
     else{
         window.location = "./admin_dash.html";
-        
+
     }
 });
