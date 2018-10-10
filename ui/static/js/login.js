59777f969f17a15bfd122ec03ff84584cf0e7a31
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-10 17:16:33 +0300

    User login page
    [ Delivers #161090124 ]

--- ui/static/js/login.js
+++ ui/static/js/login.js
@@ -1,12 +1,13 @@
 const form = document.querySelector("form");
 const username = document.querySelector("#username");
 const password = document.querySelector("password")
+
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     if(username.value !== "admin" && password!=="password"){
         window.location = "./user_dash.html";
     }
     else{
-        window.location = "./admin_dash.html"
+        window.location = "./admin_dash.html";
     }
-})
+});
