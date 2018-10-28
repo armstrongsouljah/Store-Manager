e67ae1e8fcf4c957a0107b12cc4df225d62abddf
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-28 19:31:40 +0300

    update test user password

--- app/tests/base.py
+++ app/tests/base.py
@@ -5,11 +5,11 @@ from app import app, create_app_environment
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
-        self.conn = DatabaseConnection()
         self.app.config['ENV'] = 'testing'
+        self.conn = DatabaseConnection()        
         self.client = self.app.test_client(self)
         self.user = dict(
             username='soultech',
-            password='#phoenix9q'
+            password='testing'
         )
 
