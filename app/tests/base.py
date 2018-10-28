0a95e77c94c79170dca383b80d51436c7ee30407
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-28 18:08:42 +0300

    ft(users can login) [Delivers #161527627]

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,9 +1,14 @@
 import unittest
 from databases.server import DatabaseConnection
-from app import create_app_environment
+from app import app
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app_environment('app.config.ProductionConfig')
+        self.app = app
         self.conn = DatabaseConnection()
+        self.client = self.app.test_client(self)
+        self.user = dict(
+            username='soultech',
+            password='#phoenix9q'
+        )
 
