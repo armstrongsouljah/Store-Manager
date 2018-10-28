791a5e280584689f5de7589e52558329aea98251
armstrongsouljah <armstrongsouljah@gmail.com>
2018-10-28 18:41:26 +0300

    fix travis

--- app/tests/base.py
+++ app/tests/base.py
@@ -1,11 +1,12 @@
 import unittest
 from databases.server import DatabaseConnection
-from app import app
+from app import app, create_app_environment
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.conn = DatabaseConnection()
+        self.app.config['ENV'] = 'testing'
         self.client = self.app.test_client(self)
         self.user = dict(
             username='soultech',
--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -3,4 +3,4 @@ from .base import BaseTestCase
 
 class TestDatbaseOperations(BaseTestCase):
     def test_connection_working(self):
-        self.assertTrue(self.conn)
+        self.assertTrue('connection successful', self.conn)
