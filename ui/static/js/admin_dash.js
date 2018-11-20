e4a007b3143779ef9fe6e6da43243ab5cb6187e6
armstrongsouljah <armstrongsouljah@gmail.com>
2018-11-20 19:55:32 +0300

    bg(fix preventing un authorised access)

--- ui/static/js/admin_dash.js
+++ ui/static/js/admin_dash.js
@@ -9,7 +9,7 @@ is_admin = localStorage.getItem("admin_loggedin");
 admin_token = localStorage.getItem("admin_token");
 
 if(admin_token === "null" && is_admin === "false"){
-    window.location = "/ui/"
+    window.location = "../" 
 }
 
 
